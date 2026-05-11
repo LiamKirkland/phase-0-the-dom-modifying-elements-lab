9135a2e306c2e5d473ba41bc0e90ac181d93f16d
LiamKirkland <liam.b.kirkland@gmail.com>
2026-05-11 15:15:29 -0400

    Modifying the DOM with JS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainEle = document.getElementById('main')
+mainEle.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.textContent = "Liam is the champion"
+
+document.body.append(newHeader)
